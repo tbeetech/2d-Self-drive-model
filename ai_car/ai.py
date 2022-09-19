a72dc0a8c27d91caa90897beca18609367748d3b
Oluwatobi <tobirammar@gmail.com>
2022-09-19 16:09:29 +0100

    DQN Class as been implemented with pytorch

--- ai_car/ai.py
+++ ai_car/ai.py
@@ -4,46 +4,86 @@ Created on Mon Sep 12 19:12:48 2022
 
 @author: Oluwatobi
 """
-#modules required 
-#to save and load the exprience replay memory on the system
+# modules required
+# to save and load the experience replay memory on the system
 import os
-#to sample random transitions from the experience replay memory. 
-import random0
-#import pytorch libary 
+# to sample random transitions from the experience replay memory.
+import random
+# import pytorch library
 import torch
-#to construct nueral network architecture
+# to construct neural network architecture
 import torch.nn as nn
-#to foward propagate neural network input(weight, bias)  
-import torch.nn.functional as F
-#to optimize the gradient of the loss function with respect network input parameters
+# to froward propagate neural network input(weight, bias)
+import torch.nn.functional as f
+# to optimize the gradient of the loss function with respect network input parameters
 import torch.optim as optim
 
 from torch.autograd import Variable
 
 
-
-class Network(nn.module):
+class Network(nn.Module):
     def __init__(self, input_size, nb_action):
         super().__init__()
         self.input_size = input_size
         self.nb_action = nb_action
         self.fc1 = nn.Linear(input_size, 30)
         self.fc2 = nn.Linear(30, nb_action)
-        
-    def foward(self, state):
-        x = F.relu(self.fc1(state))
+
+    def froward(self, state):
+        x = f.relu(self.fc1(state))
         q_values = self.fc2(x)
         return q_values
-             
-# class ReplayMemory(Object):
-#     def __init__(self, capacity):
-#         self.capacity = capacity
-#         self.memory = []
-#     def push(self, event):
-#         self.memory.append(event)
-#         if len(self.memory) > self.capacity:
-#             del self.memory[0]
-#         def sample(self, batch_size):
-       
-         
-            
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+    def push(self, event):
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+
+    def sample(self, batch_size):
+        samples = zip(*random.sample(self.memory, batch_size))
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
+
+class Dqn(object):
+    def __init__(self, input_size, nb_action,gamma):
+        self.gamma = gamma
+        self.model = Network(input_size, nb_action)
+        self.memory = ReplayMemory(capacity = 100000)
+        self.optimizer = optim.Adam(params = self.model.parameters())
+        self.last_state = torch.Tensor(input_size).unsqueeze(0)
+        self.last_action = 0
+        self.last_reward = 0
+
+    def select_action(self, state):
+        #multiply Q-values by 100 to avoid the issue of AIagent. uncertainty.
+        selected_actions_q_values = (self.model.foward(Variable(state)) * 100)
+        probs = f.softmax(selected_actions_q_values)
+        print(probs)
+        action = probs.multinomial(len(probs))
+        return action.data[0,0]
+
+    def learn(self, batch_states,batch_actions, batch_rewards, batch_next_states):
+        batch_outputs = self.model(batch_states).gather(1, batch_actions.unsqueeze(1)).squeeze(1)
+        batch_next_outputs = self.model(batch_next_states).detach().max(1)[0]
+        batch_targets = batch_rewards + self.gamma * batch_next_outputs
+        td_loss = f.smooth_l1_loss(batch_outputs, batch_targets)
+        self.optimizer.zero_grad()
+        td_loss.backward()
+        self.optimizer.step()
+
+    def update(self, new_state, new_reward):
+        new_state = torch.Tensor(new_state).float().unsqueeze(0)
+        self.memory.push((self.last_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward]), new_state))
+        new_action = self.select_action(new_state)
+        if len(self.memory.memory) > 100:
+            batch_states, batch_actions, batch_rewards, batch_next_states = self.memory.sample(100)
+            self.learn(batch_states, batch_actions, batch_rewards, batch_next_states)
+            self.last_state = new_state
+            self.last_action = new_action
+            self.last_reward = new_reward
+        return new_action
+
+
